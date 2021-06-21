7124537bcd63844609f78221eed9e89c09f0870c
Ricardo David <ricardo.david2020.new@gmail.com>
2021-06-21 17:13:06 +0100

    User entity and resource

---
+++ src/main/java/com/example/course1/resources/UserResource.java
@@ -0,0 +1,44 @@
+package com.example.course1.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course1.entities.User;
+
+@RestController
+@RequestMapping(value = "/users") //Este será o nome do meu recurso
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){ //O tipo d minha resposta vai ser a minha classe User;
+		
+		User u = new User(1L,"Maria", "Maria@gmail.com","999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
+
+
+
+
+
+
+
+/*Notas mportantes:
+ 
+ 1- Para falarmos que esta classe é um recurso web, e que é implementada por um controlador "REST" vamos ter que colocar uma annotaion 
+    em cima da classe;
+    
+ 2 - Vou ter que dar tambem um nome para o meu recurso e tambem colocar o valor que será o caminho para o meu recurso que estará relacionada com a entidade User;                         
+ 
+ 
+ 3- Para testar se está funcionando mesmo o recurso "Users" eu irei criar um metodo que será um "ENDPOINT" para acessar os Utilizadores;
+ 
+ 
+ 4- Para indicar que o metodo "public ResponseEntity<User> findAll() vai ser um metodo que responde a requesição do tipo get do HTTP vou ter que colocar em cima do metodo um Annotaion do tipo GetMapping 
+ 
+ 
+  */
+ 
